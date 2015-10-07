78c68d68a8234593c977333b013fedc8db5d9b97
Jared Davis <jared@haiq.us>
2015-10-06 20:52:39 -0500

    complete some exercises
    problems 14, 15, 16, 17. problem 13 is skipped as it closely resembles
    problem 11

---
+++ Haskell99.hsproj/ContinuedListExercise/Problem14.hs
@@ -0,0 +1,7 @@
+module ContinuedListExercise.Problem14 where
+
+-- here's a direct solution, taking advantage of cons (:)
+duplicate :: [a] -> [a]
+duplicate [] = []
+duplicate [x] = [x, x]
+duplicate (x:xs) = (x:(x:duplicate(xs)))
---
+++ Haskell99.hsproj/ContinuedListExercise/Problem16.hs
@@ -0,0 +1,15 @@
+module ContinuedListExercise.Problem16 where
+
+dropElem :: [a] -> Int -> [a]
+dropElem [] _ = []
+dropElem (x:xs) n
+  | n == 0    = xs
+  | otherwise = x:(dropElem xs (n - 1))
+
+-- I'm actualy suprised that this converges
+-- I suppose the first pattern match ensures that, if we reach the end of the list, we return
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery (x:xs) n
+  | n == 0    = dropEvery xs (n + 1)
+  | otherwise = x:(dropEvery xs (n - 1))
